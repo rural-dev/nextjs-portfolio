56095d7847e24fde5974b42c9f481bd1296578be
Mac <mac@192.168.0.101>
2022-01-16 23:16:05 +0700

    add mobile menu

--- components/Header.js
+++ components/Header.js
@@ -6,10 +6,33 @@ const Header = () => {
   const ScrollToBottom = () => {
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
   }
+  const showMobileMenu = () => {
+    const menu = document.querySelector(".mobile-menu");
+    menu.classList.toggle("hidden");
 
+  }
     return (
+      <>
         <nav className="flex flex-row flex-end py-8 px-20">
+        <div class="md:hidden flex mr-auto items-center">
+        <button class="outline-none mobile-menu-button" onClick={showMobileMenu}>
+          <svg
+            class="w-6 h-6 text-gray-500"
+            x-show="!showMenu"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="4"
+            viewBox="0 0 24 24"
+            stroke="#A93DE9"
+          >
+          <path d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+        </div>
+        <div className="hidden lg:flex">
         <Image src="/logo.svg" alt="Logo" width={44} height={32}/>
+        </div>
         <ul className="hidden lg:flex flex-row rounded-md items-center h-16 bg-gradient-to-r from-bg1 to-transparent py-2 ml-8 pr-8">
           <li className="text-base font-semibold font-body text-violet pl-8">
             <Link href="/">
@@ -28,8 +51,29 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        <button className="ml-auto rounded-md text-violet border-2 border-violet text-base font-semibold font-body px-5">Download CV</button>
+        
+        <button className="flex ml-auto rounded-md text-violet border-2 border-violet text-base font-semibold font-body px-5">Download CV</button>
+        
       </nav>
+      <div class="hidden mobile-menu">
+      <ul class="">
+        <li class="active">
+        <Link href="/">
+          <a class="block text-sm px-2 py-4 text-white bg-violet font-semibold">Home</a>
+        </Link>
+        </li>
+        <li>
+        <Link href="/projects">
+              <a class="block text-sm px-2 py-4 hover:bg-violet transition duration-300">Projects</a>
+            </Link></li>
+        <li><a onClick={ScrollToBottom} class="block text-sm px-2 py-4 hover:bg-violet transition duration-300">Contact Us</a></li>
+        <li>
+        <Link href="/test">
+              <a class="block text-sm px-2 py-4 hover:bg-violet transition duration-300">About</a>
+            </Link></li>
+      </ul>
+    </div>
+    </>
     );
   }
 
